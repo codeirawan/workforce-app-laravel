c4374ad30596c37bfceb6905d507d693c4afba9a
codeirawan <codeirawan@gmail.com>
2023-09-11 17:46:49 +0700

    feat: add summary schedule on dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,10 +2,66 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Schedule\ScheduleShift;
+use Auth;
+
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('dashboard');
+        $today = date('Y-m-d'); // Get the current date in 'Y-m-d' format
+
+        // Calculate the start date (Monday) and end date (Sunday) of the current week
+        $startOfWeek = date('Y-m-d', strtotime('last Monday', strtotime($today)));
+        $endOfWeek = date('Y-m-d', strtotime('next Sunday', strtotime($today)));
+
+        // dd($today, $startOfWeek, $endOfWeek);
+
+        $getSchedules = ScheduleShift::select(
+            'users.nik',
+            'users.name',
+            'users.gender',
+            'users.religion',
+            'master_shift.name AS shift',
+            'schedule_shift.date',
+            'schedule_shift.start_time',
+            'schedule_shift.end_time',
+            'schedule_shift.id AS schedule_shift',
+            'schedule_period.id AS schedule_period',
+            'schedule_period.publish',
+        )
+            ->leftJoin('users', 'users.id', '=', 'schedule_shift.agent_id')
+            ->leftJoin('master_shift', 'master_shift.id', '=', 'schedule_shift.shift_id')
+            ->leftJoin('schedule_period', 'schedule_period.id', '=', 'schedule_shift.schedule_id')
+            ->leftJoin('params', 'params.id', '=', 'schedule_period.forecast_id')
+            ->groupBy(
+                'users.nik',
+                'users.name',
+                'users.gender',
+                'users.religion',
+                'master_shift.name',
+                'schedule_shift.date',
+                'schedule_shift.start_time',
+                'schedule_shift.end_time',
+                'schedule_shift.id',
+                'schedule_period.id',
+                'schedule_period.publish',
+            )
+            ->where(function ($query) {
+                $query->where('params.skill_id', Auth::user()->skill_id)
+                    ->orWhere(function ($subquery) {
+                        $subquery->where('users.supervisor_id', '=', Auth::user()->nik);
+                    })
+                    ->orWhere(function ($subquery) {
+                        $subquery->where('users.team_leader_id', '=', Auth::user()->nik);
+                    });
+            })
+            ->whereDate('params.start_date', '>=', $startOfWeek)
+            ->whereDate('params.end_date', '<=', $endOfWeek)
+            ->orderBy('schedule_shift.date')
+            ->orderBy('schedule_shift.start_time')
+            ->get();
+
+        return view('dashboard', compact('getSchedules'));
     }
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -13,8 +13,127 @@
         <div class="kt-portlet__body">
             <div class="kt-portlet__content">
                 @include('layouts.inc.alert')
-                {{ __('Hi') }} {{ auth()->user()->name }}
+                @if ($getSchedules->isEmpty())
+                    {{ __('Hi') }} {{ auth()->user()->name }}
+                @else
+                    <div class="card card-custom gutter-b">
+                        <div class="form-group mb-0">
+                            <div class="input-group">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text">
+                                        <i class="fa-solid fa-magnifying-glass"></i>
+                                    </span>
+                                </div>
+                                <input type="text" class="form-control" id="filter" placeholder="Search agent name...">
+                            </div>
+                        </div>
+
+                        @php
+                            // Initialize an empty multidimensional array to store agent shifts by date
+                            $agentShiftsByDate = [];
+                            
+                            // Iterate through $getSchedules to collect agent shifts by date
+                            foreach ($getSchedules as $schedule) {
+                                $agentNik = $schedule->nik;
+                                $date = date('d-m-y', strtotime($schedule->date));
+                                $shift = $schedule->shift;
+                            
+                                // Create a new array for the agent if it doesn't exist
+                                if (!isset($agentShiftsByDate[$agentNik])) {
+                                    $agentShiftsByDate[$agentNik] = [];
+                                }
+                            
+                                // Assign the shift for the date to the agent
+                                $agentShiftsByDate[$agentNik][$date] = $shift;
+                            }
+                        @endphp
+
+                        @php
+                            // Collect unique dates from $getSchedules
+                            $uniqueDates = [];
+                            foreach ($getSchedules as $schedule) {
+                                $date = date('d-m-y', strtotime($schedule->date));
+                                if (!in_array($date, $uniqueDates)) {
+                                    $uniqueDates[] = $date;
+                                }
+                            }
+                        @endphp
+
+
+                        <div class="card-body table-responsive">
+                            <table class="table table-striped table-bordered text-center">
+                                <thead>
+                                    <tr>
+                                        <th>ID</th>
+                                        <th>Name</th>
+                                        <th>Gender</th>
+                                        <th>Religion</th>
+                                        @foreach ($uniqueDates as $date)
+                                            <th>{{ $date }}</th>
+                                        @endforeach
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <!-- Loop through unique agents and display agent details -->
+                                    @foreach ($agentShiftsByDate as $agentNik => $agentShifts)
+                                        @php
+                                            // Retrieve agent details based on $agentNik
+                                            $agentDetails = \App\User::where('nik', $agentNik)->first();
+                                        @endphp
+
+                                        <tr class="schedule-row"
+                                            data-filter="{{ $agentDetails->nik }} {{ $agentDetails->name }} {{ $agentDetails->gender }} {{ $agentDetails->religion }}">
+                                            <td>{{ $agentDetails->nik }}</td>
+                                            <td>{{ $agentDetails->name }}</td>
+                                            <td>
+                                                @if ($agentDetails->gender === 'Male')
+                                                    <span class="badge badge-primary">Male</span>
+                                                @else
+                                                    <span class="badge badge-danger">Female</span>
+                                                @endif
+                                            </td>
+                                            <td>{{ $agentDetails->religion }}</td>
+                                            @foreach ($uniqueDates as $date)
+                                                <td>
+                                                    @if (isset($agentShifts[$date]))
+                                                        {{ $agentShifts[$date] }}
+                                                    @else
+                                                        No Shift
+                                                    @endif
+                                                </td>
+                                            @endforeach
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+
+
+                    </div>
+                @endif
             </div>
         </div>
     </div>
+    <script>
+        // Get the input element
+        const filterInput = document.getElementById('filter');
+
+        // Get all rows in the table
+        const rows = document.querySelectorAll('.schedule-row');
+
+        // Add an event listener to the input field
+        filterInput.addEventListener('input', function() {
+            const filterText = this.value.toLowerCase();
+
+            // Loop through rows and hide those that don't match the filter text
+            rows.forEach(row => {
+                const rowData = row.getAttribute('data-filter').toLowerCase();
+                if (rowData.includes(filterText)) {
+                    row.style.display = ''; // Show the row
+                } else {
+                    row.style.display = 'none'; // Hide the row
+                }
+            });
+        });
+    </script>
 @endsection
